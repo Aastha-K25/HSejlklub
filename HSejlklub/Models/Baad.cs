a13b6bd1d5cc354dd631a7a92a6119d3bcac7fae
Eylem003 <eyy001@edu.zealand.dk>
2025-05-01 20:10:22 +0200

    Medlemsliste

--- HSejlklub/Models/Baad.cs
+++ HSejlklub/Models/Baad.cs
@@ -1,6 +1,42 @@
-namespace HSejlklub.Models;
-
-public class Baad
+namespace Hillerød_Sejklub.Models
 {
-    
+    // Model for en båd
+    public class Boat
+    {
+        public int BoatId { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string SailNumber { get; set; }
+        public string EngineInfo { get; set; }
+        public int BuildYear { get; set; }
+        public bool IsAvailable { get; set; }
+        public string ImagePath { get; set; }
+        public int BookingID { get; set; }
+        public int BådID { get; set; }
+        public int MedlemsID { get; set; }
+        public DateTime SejlTid { get; set; }
+        public string Mødested { get; set; }
+
+        public Boat(int boatId, string name, string model, string sailNumber, string engineInfo, int buildYear, bool isAvailable, string imagePath)
+        {
+            BoatId = boatId;
+            Name = name;
+            Model = model;
+            SailNumber = sailNumber;
+            EngineInfo = engineInfo;
+            BuildYear = buildYear;
+            IsAvailable = isAvailable;
+            ImagePath = imagePath;
+        }
+
+        public Boat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Model}, {SailNumber}, {EngineInfo}, {BuildYear}, {IsAvailable}, {ImagePath}";
+        }
+    }
 }
--- HSejlklub/Models/Repositories/Baaderne.cs
+++ HSejlklub/Models/Repositories/Baaderne.cs
@@ -1,6 +1,46 @@
-namespace HSejlklub.Models.Repositories;
+using System.Collections.Generic;
+using Hillerød_Sejklub.Models;
 
-public class Baaderne
+namespace Hillerød_Sejklub.Models.Repositories
 {
-    
+    // Repository til at håndtere både
+    public class BoatRepository
+    {
+        private static List<Boat> _boats = new List<Boat>();
+
+        public void Add(Boat boat)
+        {
+            _boats.Add(boat);
+        }
+
+        public List<Boat> GetAll()
+        {
+            return _boats;
+        }
+
+        public Boat FindById(int id)
+        {
+            foreach (Boat b in _boats)
+            {
+                if (b.BoatId == id)
+                {
+                    return b;
+                }
+            }
+            return null;
+        }
+
+        public List<Boat> SearchByModel(string model)
+        {
+            List<Boat> result = new List<Boat>();
+            foreach (Boat b in _boats)
+            {
+                if (b.Model.ToLower().Contains(model.ToLower()))
+                {
+                    result.Add(b);
+                }
+            }
+            return result;
+        }
+    }
 }
