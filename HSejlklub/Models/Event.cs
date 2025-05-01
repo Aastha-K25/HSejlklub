b3506c9078f793ec2823995ed61a8c5528cd0733
Aastha-K25 <aak001@edu.zealand.dk>
2025-05-01 17:31:15 +0200

    Begivenheder
    prøver backenddel

--- HSejlklub/Models/Event.cs
+++ HSejlklub/Models/Event.cs
@@ -2,5 +2,7 @@ namespace HSejlklub.Models;
 
 public class Event
 {
-    
+    public int Id { get; set; }
+    public string EventType { get; set; }
+    public DateTime EventDate { get; set; }
 }
--- HSejlklub/Models/Registration.cs
+++ HSejlklub/Models/Registration.cs
@@ -1,6 +1,8 @@
 namespace HSejlklub.Models;
 
-public class Registration
-{
-    
-}
+ public class Registration
+    {
+        public string Email { get; set; }
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+    }
