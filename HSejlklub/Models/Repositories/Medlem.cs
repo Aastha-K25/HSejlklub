62376dcf5fe4de8596920821cd1b397d5ed97674
Eylem003 <eyy001@edu.zealand.dk>
2025-05-01 21:04:58 +0200

    Medlemsliste

---
+++ HSejlklub/Models/Repositories/Medlem.cs
@@ -0,0 +1,64 @@
+using HSejlklub.Models;
+
+namespace HSejlklub.Models.Repositories
+{
+    public class MedlemRepository   // listen over medlemmer (Repository)
+    {
+        public static List<Medlem> HentAlle()
+        {
+            return new List<Medlem>
+            {
+                new Medlem { Fornavn = "Emma", Efternavn = "Hansen", Email = "emma.hansen@mail.dk", ErAktiv = true },
+                new Medlem { Fornavn = "Lucas", Efternavn = "Møller", Email = "lucas.m@outlook.com", ErAktiv = false },
+                new Medlem { Fornavn = "Freja", Efternavn = "Andersen", Email = "freja_a@gmail.com", ErAktiv = true },
+                new Medlem
+                {
+                    Fornavn = "William", Efternavn = "Jensen", Email = "williamj@hotmail.com", ErAktiv = true
+                },
+                new Medlem { Fornavn = "Ida", Efternavn = "Larsen", Email = "ida.l@live.dk", ErAktiv = false },
+                new Medlem { Fornavn = "Mads", Efternavn = "Dalberg", Email = "madsdalb@hotmail.com", ErAktiv = true },
+                new Medlem
+                {
+                    Fornavn = "Sophie", Efternavn = "Pedersen", Email = "sophie2004@hotmail.com", ErAktiv = false
+                },
+                new Medlem { Fornavn = "Jonas", Efternavn = "Nielsen", Email = "jonas@hotmail.com", ErAktiv = true },
+                new Medlem
+                {
+                    Fornavn = "Oliver", Efternavn = "Sørensen", Email = "oliversørnsen@gmail.com", ErAktiv = true
+                },
+                new Medlem
+                {
+                    Fornavn = "Laura", Efternavn = "Hansen", Email = "Laurahannsen@outlook.dk", ErAktiv = true
+                },
+                new Medlem { Fornavn = "Emil", Efternavn = "Rasmussen", Email = "emil003@hotmail.com", ErAktiv = true },
+                new Medlem
+                {
+                    Fornavn = "Clara", Efternavn = "holm", Email = "claraholm2003@hotmail.com", ErAktiv = true
+                },
+                new Medlem
+                {
+                    Fornavn = "Victor", Efternavn = "Østergaard", Email = "voestergaard@gmail.com", ErAktiv = false
+                },
+                new Medlem { Fornavn = "Anna", Efternavn = "Lund", Email = "annalund@hotmail.com", ErAktiv = true },
+                new Medlem
+                {
+                    Fornavn = "Mathias", Efternavn = "Clausen", Email = "mathiasclaus@gmail.com", ErAktiv = false
+                },
+                new Medlem
+                {
+                    Fornavn = "Caroline", Efternavn = "Bjerre", Email = "carobje22@gmail.com", ErAktiv = true
+                },
+                new Medlem { Fornavn = "Alberte", Efternavn = "Hviid", Email = "alb2005@hotmail.com", ErAktiv = true },
+                new Medlem
+                {
+                    Fornavn = "William", Efternavn = "Skov", Email = "Williamskov@hotmail.com", ErAktiv = true
+                },
+                new Medlem
+                {
+                    Fornavn = "Josefine", Efternavn = "Laursen", Email = "joselaur@hotmail.com", ErAktiv = true
+                },
+            };
+        }
+    }
+}
+// Medlems liste
--- HSejlklub/Pages/Medlemmer.cshtml.cs
+++ HSejlklub/Pages/Medlemmer.cshtml.cs
@@ -1,66 +1,18 @@
-using Microsoft.AspNetCore.Mvc.RazorPages; 
-using HSejlklub.Models; // Husk using for brug af medlem
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HSejlklub.Models;
+using HSejlklub.Models.Repositories;
 
-namespace HSejlklub.Models.Repositories;
-// Repository delen
-public class Medlemmer : PageModel
+
+namespace HSejlklub.Pages
 {
-    public List<Medlem> Medlem { get; set; }
-
-    public void OnGet()
+    public class MedlemslisteModel : PageModel
     {
-        Medlem = new List<Medlem>
+        public List<Medlem> Medlem{ get; set; }
+
+        public void OnGet()
         {
-            new Medlem { Fornavn = "Emma", Efternavn = "Hansen", Email = "emma.hansen@mail.dk", ErAktiv = true },
-            new Medlem { Fornavn = "Lucas", Efternavn = "Møller", Email = "lucas.m@outlook.com", ErAktiv = false },
-            new Medlem { Fornavn = "Freja", Efternavn = "Andersen", Email = "freja_a@gmail.com", ErAktiv = true },
-            new Medlem
-            {
-                Fornavn = "William", Efternavn = "Jensen", Email = "williamj@hotmail.com", ErAktiv = true
-            },
-            new Medlem { Fornavn = "Ida", Efternavn = "Larsen", Email = "ida.l@live.dk", ErAktiv = false },
-            new Medlem { Fornavn = "Mads", Efternavn = "Dalberg", Email = "madsdalb@hotmail.com", ErAktiv = true },
-            new Medlem
-            {
-                Fornavn = "Sophie", Efternavn = "Pedersen", Email = "sophie2004@hotmail.com", ErAktiv = false
-            },
-            new Medlem { Fornavn = "Jonas", Efternavn = "Nielsen", Email = "jonas@hotmail.com", ErAktiv = true },
-            new Medlem
-            {
-                Fornavn = "Oliver", Efternavn = "Sørensen", Email = "oliversørnsen@gmail.com", ErAktiv = true
-            },
-            new Medlem
-            {
-                Fornavn = "Laura", Efternavn = "Hansen", Email = "Laurahannsen@outlook.dk", ErAktiv = true
-            },
-            new Medlem { Fornavn = "Emil", Efternavn = "Rasmussen", Email = "emil003@hotmail.com", ErAktiv = true },
-            new Medlem
-            {
-                Fornavn = "Clara", Efternavn = "holm", Email = "claraholm2003@hotmail.com", ErAktiv = true
-            },
-            new Medlem
-            {
-                Fornavn = "Victor", Efternavn = "Østergaard", Email = "voestergaard@gmail.com", ErAktiv = false
-            },
-            new Medlem { Fornavn = "Anna", Efternavn = "Lund", Email = "annalund@hotmail.com", ErAktiv = true },
-            new Medlem
-            {
-                Fornavn = "Mathias", Efternavn = "Clausen", Email = "mathiasclaus@gmail.com", ErAktiv = false
-            },
-            new Medlem
-            {
-                Fornavn = "Caroline", Efternavn = "Bjerre", Email = "carobje22@gmail.com", ErAktiv = true
-            },
-            new Medlem { Fornavn = "Alberte", Efternavn = "Hviid", Email = "alb2005@hotmail.com", ErAktiv = true },
-            new Medlem
-            {
-                Fornavn = "William", Efternavn = "Skov", Email = "Williamskov@hotmail.com", ErAktiv = true
-            },
-            new Medlem
-            {
-                Fornavn = "Josefine", Efternavn = "Laursen", Email = "joselaur@hotmail.com", ErAktiv = true
-            },
-        };
+            Medlem = MedlemRepository.HentAlle();
+        }
     }
 }
-// Medlems liste
+    
