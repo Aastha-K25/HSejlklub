ea6ffcceec56ad866fff53f6d560e7e0b486a02f
Eylem003 <eyy001@edu.zealand.dk>
2025-05-01 14:49:01 +0200

    Medlem - Backend del

--- HSejlklub/Models/Repositories/Medlem.cs
+++ HSejlklub/Models/Repositories/Medlem.cs
@@ -1,10 +1,24 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
 namespace HSejlklub.Models.Repositories;
-
-
-    public class Medlem
+// Repository delen
+    public class Medlemmer : PageModel
     {
-        public string Fornavn { get; set; }
-        public string Efternavn { get; set; }
-        public string Email { get; set; }
-        public bool ErAktiv { get; set; }
-    }  
+        public List<Medlem> Medlem { get; set; }
+
+        public void OnGet()
+        {
+            Medlem = new List<Medlem>
+            {
+                new Medlem { Fornavn = "Emma", Efternavn = "Hansen", Email = "emma.hansen@mail.dk", ErAktiv = true },
+                new Medlem { Fornavn = "Lucas", Efternavn = "Møller", Email = "lucas.m@outlook.com", ErAktiv = false },
+                new Medlem { Fornavn = "Freja", Efternavn = "Andersen", Email = "freja_a@gmail.com", ErAktiv = true },
+                new Medlem { Fornavn = "William", Efternavn = "Jensen", Email = "williamj@hotmail.com", ErAktiv = true },
+                new Medlem { Fornavn = "Ida", Efternavn = "Larsen", Email = "ida.l@live.dk", ErAktiv = false },
+                new Medlem {Fornavn = "Mads", Efternavn = "Dalberg", Email = "madsdalb@hotmail.com", ErAktiv = true},
+                new Medlem { Fornavn = "Sophie", Efternavn = "Pedersen", Email = ""}
+            };
+        }
+    }
+
+// Medlems liste
