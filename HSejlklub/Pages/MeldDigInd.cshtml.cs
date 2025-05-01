b9afc5bed955757f4ce50343a738fdb0222e7316
Aastha-K25 <aak001@edu.zealand.dk>
2025-05-01 13:50:29 +0200

    Backend del

--- HSejlklub/Pages/MeldDigInd.cshtml.cs
+++ HSejlklub/Pages/MeldDigInd.cshtml.cs
@@ -14,5 +14,20 @@ public class PrivacyModel : PageModel
 
     public void OnGet()
     {
+        
     }
+}
+
+public class MedlemModel
+{
+    public string Medlemskab { get; set; } // F.eks. "Junior", "Senior"
+    public string Navn { get; set; }
+    public string Email { get; set; }
+    public string Telefon { get; set; }
+    public DateTime Fødselsdato { get; set; }
+    public string Køn { get; set; } // Mand, Kvinde, Ønsker ikke at oplyse
+    public string Adresse1 { get; set; }
+    public string Adresse2 { get; set; }
+    public string By { get; set; }
+    public string Postnummer { get; set; }
 }
