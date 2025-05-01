caf3c12bd413217f0270f8020f69cd96f7dcc9e9
Eylem003 <eyy001@edu.zealand.dk>
2025-05-01 13:57:30 +0200

    Medlem

---
+++ HSejlklub/Models/Repositories/Medlem.cs
@@ -0,0 +1,10 @@
+namespace HSejlklub.Models.Repositories;
+
+
+    public class Medlem
+    {
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public string Email { get; set; }
+        public bool ErAktiv { get; set; }
+    }  
